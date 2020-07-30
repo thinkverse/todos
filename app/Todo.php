3484b2b9bb27c3242c16da8481b7c5c97f43c523
thinkverse <hallberg.kim@gmail.com>
2020-07-30 07:47:41 +0200

    Add Todo -> User relationship

--- app/Todo.php
+++ app/Todo.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
-    //
+    /**
+     * Get associated user
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(\App\User::class);
+    }
 }
